ded1a6da4c337eefde1a1f5765f4eb972c2d1435
tobidasage <oluwatobidedeke@gmail.com>
2024-09-07 21:40:34 -0400

    sending the latest for this file to the repo

---
+++ valid_anagram.py
@@ -0,0 +1,25 @@
+class solution(object):
+    def validAnagram(self, s, t):
+        
+        if len(s) != len(t):
+            print('False')
+            return False
+        
+        my_list1 = list(s)
+        my_list2 = list(t)
+        
+        for i in range(len(my_list1)):
+            if my_list1[i] in my_list2:
+                my_list2.remove(my_list1[i])
+        
+        if not my_list2:
+            print('True')
+            return True
+        
+        print('False')
+        return False
+
+word1 = 'anagram'
+word2 = 'nagamar'
+
+solution().validAnagram(word1, word2)
